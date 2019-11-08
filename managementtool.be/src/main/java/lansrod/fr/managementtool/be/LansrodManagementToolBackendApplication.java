7a7e38081ed7902eb7e21b209bcdc098ad43296c
Anis Halayem <anis.halayem@gmail.com>
2019-11-08 15:53:05 +0100

    First commit

---
+++ managementtool.be/src/main/java/lansrod/fr/managementtool/be/LansrodManagementToolBackendApplication.java
@@ -0,0 +1,15 @@
+package lansrod.fr.managementtool.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class LansrodManagementToolBackendApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(LansrodManagementToolBackendApplication.class, args);
+    }
+}
---
+++ managementtool.be/src/main/java/lansrod/fr/managementtool/be/providers/controller/ProvidersController.java
@@ -0,0 +1,17 @@
+package lansrod.fr.managementtool.be.providers.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProvidersController {
+	
+	@CrossOrigin
+	@GetMapping("/ping")
+	public String pong() {
+		return "Pong at " + LocalDateTime.now().toString();
+	}
+}
