180ff224837628c753a958de44fa979d83c81813
Anis Halayem <anis.halayem@gmail.com>
2019-12-11 11:31:08 +0100

    swagger codegen: use java8 and remove default api implementation

--- src/main/java/managementtool/be/employee/controller/ProvidersController.java
+++ src/main/java/managementtool/be/employee/controller/ProvidersController.java
@@ -1,11 +1,14 @@
 package managementtool.be.employee.controller;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import generated.managementtool.be.employee.api.ServiceProvidersApi;
 import generated.managementtool.be.employee.dto.PersonalInformation;
 import generated.managementtool.be.employee.dto.ResourceInformation;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import managementtool.be.commun.builder.ResourceInformationBuilder;
 import managementtool.be.employee.mapper.EmployeeMapper;
 import managementtool.be.employee.service.ProviderService;
@@ -14,13 +17,17 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-@RestController
 @AllArgsConstructor
+@Getter
+@RestController
 public class ProvidersController implements ServiceProvidersApi {
 
-	private ProviderService providerService;
+	private final ProviderService 				providerService;
+	private final Optional<ObjectMapper> 		objectMapper;
+	private final Optional<HttpServletRequest>  request;
 
 	@Override
 	public ResponseEntity<PersonalInformation> serviceProvidersIdGet( Long id ) {
--- src/main/java/managementtool/be/skills/controller/SkillsController.java
+++ src/main/java/managementtool/be/skills/controller/SkillsController.java
@@ -1,21 +1,30 @@
 package managementtool.be.skills.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import generated.managementtool.be.skills.api.SkillsApi;
+import lombok.Getter;
 import managementtool.be.skills.mapper.SkillMapper;
 import generated.managementtool.be.skills.dto.Skills;
 import managementtool.be.skills.service.SkillService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
 @AllArgsConstructor
+@Getter
 @RestController
 public class SkillsController implements SkillsApi {
 
-    private SkillService skillService;
+    private final SkillService skillService;
 
     @Override
     public ResponseEntity<Skills> skillsGet() {
         return ResponseEntity.ok( SkillMapper.mapModelToDto( skillService.getSkills() ) );
     }
+
+    private final Optional<ObjectMapper>        objectMapper;
+    private final Optional<HttpServletRequest>  request;
 }
