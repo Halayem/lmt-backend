2eeec341ceb083afd5ac25a892e35ed41cc31fb5
Anis Halayem <anis.halayem@gmail.com>
2019-12-12 13:24:26 +0100

    Feature/get project information (#3)
    * Add dependencies apache utils
    
    * Get project by id
    
    * Add new line in the end of yaml files
    
    * Implement get project information
    
    * Fix bug: set media type response

--- src/main/java/managementtool/be/project/service/impl/ProjectServiceImpl.java
+++ src/main/java/managementtool/be/project/service/impl/ProjectServiceImpl.java
@@ -1,6 +1,7 @@
 package managementtool.be.project.service.impl;
 
 import lombok.AllArgsConstructor;
+import managementtool.be.project.builder.dto.ProjectBuilder;
 import managementtool.be.project.model.Project;
 import managementtool.be.project.model.ProjectProfile;
 import managementtool.be.project.model.ProjectSkill;
@@ -8,9 +9,11 @@ import managementtool.be.project.repository.ProjectProfileRepository;
 import managementtool.be.project.repository.ProjectRepository;
 import managementtool.be.project.repository.ProjectSkillRepository;
 import managementtool.be.project.service.ProjectService;
+import org.apache.commons.collections4.IterableUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @AllArgsConstructor
 public class ProjectServiceImpl implements ProjectService {
@@ -25,6 +28,36 @@ public class ProjectServiceImpl implements ProjectService {
         projectSkillRepository.saveAll  ( buildProjectSkills    ( projectId, skillIds     ) );
         projectProfileRepository.saveAll( buildProjectProfiles  ( projectId, profileIds   ) );
         return projectId;
+    }
+
+    @Override
+    public generated.managementtool.be.project.dto.Project getProjectById ( Long projectId ) {
+        return
+            projectRepository.findById      ( projectId )
+                             .map           ( project -> new ProjectBuilder()
+                                                             .withId          ( project.getId()             )
+                                                             .withEntitle     ( project.getEntitle()        )
+                                                             .withDescription ( project.getDescription()    )
+                                                             .withStartDate   ( project.getStartDate()      )
+                                                             .withEndDate     ( project.getEndDate()        )
+                                                             .withSkillIds    (
+                                                                     StreamSupport.stream(
+                                                                             projectSkillRepository.findByProjectId( projectId )
+                                                                                                   .spliterator(),
+                                                                             false)
+                                                                     .map               ( projectSkill -> projectSkill.getSkillId() )
+                                                                     .collect           ( Collectors.toList() )
+                                                             )
+                                                             .withProfileIds  (
+                                                                     StreamSupport.stream(
+                                                                             projectProfileRepository.findByProjectId( projectId )
+                                                                                                     .spliterator(),
+                                                                             false)
+                                                                             .map         ( projectProfile -> projectProfile.getProfileId() )
+                                                                             .collect     ( Collectors.toList() )
+                                                             )
+                                                             .build()
+                                ).orElse( null );
     }
 
     private List<ProjectSkill> buildProjectSkills( final Long projectId, final List<Long> skillIds ) {
